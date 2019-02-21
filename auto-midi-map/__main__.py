e8609e65b95c3bb44390f64fa08f515b3c8905f2
Bernard Rodrigue <bernard.rodrigue+github@gmail.com>
2019-02-20 20:06:42 -0500

    Fill algorithm is ready

--- auto-midi-map/__main__.py
+++ auto-midi-map/__main__.py
@@ -2,7 +2,7 @@
 
 from waapi import WaapiClient, CannotConnectToWaapiException
 from pprint import pprint
-import sys, re
+import sys, re, math
 notes = ['c','c#','d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
 
 def note_name_to_number(name):
@@ -26,9 +26,7 @@ try:
         selected  = client.call("ak.wwise.ui.getSelectedObjects")['objects']
 
         if len(selected) == 0:
-            sys.exit('no object selected')
-
-        pprint(selected)
+            raise Exception('Please select an object')
 
         # RPC with options
         # return an array of all children objects in the default actor-mixer work-unit
@@ -42,15 +40,44 @@ try:
             "return": ['id', 'name','type']
         }
         children = client.call("ak.wwise.core.object.get", args, options=options)['return']
-        pprint(children)
 
+        errors = []
         for child in children:
-            print(child['name'])
-            match = re.search('(?P<note>[cdefgabCDEFGAB]#?[0-9]+)', child['name'])
-            if match is not None:
-                print(note_name_to_number(match.group('note')))
-                
+            name = child['name']
+
+            match = re.search('(?P<note>[cdefgabCDEFGAB]#?[0-9]+)', name)
+            if match is None:
+                errors.append('Could not find a note in ' + name)
+            else:
+                child['note'] = note_name_to_number(match.group('note'))
+
+        if len(errors) > 0:
+            raise Exception('\n'.join(errors))
+        
+        # Try to fill whole between notes
+        children.sort(key=lambda object: object['note'])
+        
+        i = 0
+        for child in children:
+            min = 0
+            max = 127
+            if i != 0:
+                min = children[i - 1]['@MidiKeyFilterMax'] + 1
+            if i != len(children) - 1:
+                max = children[i]['note'] + math.floor((children[i + 1]['note'] - children[i]['note']) / 2)
+
+            child['@MidiKeyFilterMin'] = min
+            child['@MidiKeyFilterMax'] = max
+            max = 127
+
+            i += 1
+
+        pprint(children)
 
 
 except CannotConnectToWaapiException:
     print("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")
+
+except Exception as e:
+    print(str(e))
+    input("Press key to continue...")
